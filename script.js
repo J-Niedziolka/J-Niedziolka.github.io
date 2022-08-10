0579d0a4cf843e1046f540db85675887a10b06f3
J-Niedziolka <jn85638@stud.uph.edu.pl>
2022-08-10 21:08:49 +0200

    v.0.2.1 develop
    fixed list bug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#add").on("click", function(){
         let calendar_content = $("#item").val();
-        if (cal_item !== ''){
+        if (calendar_content !== ''){
             let li_element = $("<li></li>").text(calendar_content);
             $(li_element).append("<button class='rem'>X</button>");
             $("#mylist").append(li_element);
