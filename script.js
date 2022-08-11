622c302470e71daa01fae8360ceda5df75c2de1a
J-Niedziolka <jn85638@stud.uph.edu.pl>
2022-08-11 02:27:39 +0200

    v.0.3. develop
    - full functioning add/remove engine
    started:
    - drop down list feature
    - animation feature

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ $(function(){
         let calendar_content = $("#item").val();
         if (calendar_content !== ''){
             let li_element = $("<li></li>").text(calendar_content);
-            $(li_element).append("<button class='rem'>X</button>");
+            $(li_element).append("<button class='rem' id='but'>X</button>");
             $("#mylist").append(li_element);
             $("#item").val("");
 
+
         }
+
+        $(".rem").on("click", function(){
+            $(this).parent().animate({
+                
+            }, 1000);
+        });
+
+        $(".rem").on("click", function(){
+            $(this).parent().fadeOut(1000);
+            //$(this).parent().remove();  
+        });
     });
 })
