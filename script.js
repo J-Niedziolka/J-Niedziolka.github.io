cfda7b120f379bc66df4ecc52951e50e980e6aa4
J-Niedziolka <jn85638@stud.uph.edu.pl>
2022-08-16 19:25:26 +0200

    v.0.5. added web storage objects
    NOT TESTED YET!!!

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 $(function(){
-    $("#add").on("click", function(){
+    /*
+    <div id="result"></div>
+    <script>
+    // Check browser support
+    if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+    </script>*/
+    if (typeof(Storage) !== "undefined") {
+        $("#add").on("click", function(){
+            //alert('breakpoint8');
+            //let calendar_content = $("#item").val(); //tutaj sprawdzic
+            if ($("#item").val() !== '') {
+                //alert('breakpoint1');
+                localStorage.setItem('calendarContent', $("#item").val());
+                localStorage.setItem('detailsContent', $("#details").val());
+                localStorage.setItem('priority', $("#priority").val());
+                let li_element = $("<li class='pos'></li>").text(localStorage.getItem("calendarContent"));
+                
+                //alert('breakpoint2');
+                $(li_element).append("<p id='fill'></p>");
+                $(li_element).append("<button class='rem' id='but'>X</button>");
+                $(li_element).addClass(localStorage.getItem('priority'));
+
+                //alert('breakpoint3');
+                $("fill").text()
+                $("#mylist").append(li_element);
+                $("#details").val("");
+                $("#item").val("");  
+            }
+
+            //alert('breakpoint4');
+            $(".rem").on("click", function(){
+                //alert('breakpoint5');
+                $(this).parent().fadeOut(1000);
+                //$(this).parent().remove();  
+            });
+        });
+      } 
+      else {
+        // Sorry! No Web Storage support..
+        alert('przypał...');
+      }
+
+
+    /*$("#add").on("click", function(){
         let calendar_content = $("#item").val();
         if (calendar_content !== ''){
             let li_element = $("<li class='pos'></li>").text(calendar_content);
@@ -18,11 +68,11 @@ $(function(){
             $(this).parent().animate({
                 filter: "drop-shadow(8px 8px 10px gray)"
             }, 1000);
-        });*/
+        });
 
         $(".rem").on("click", function(){
             $(this).parent().fadeOut(1000);
             //$(this).parent().remove();  
         });
-    });
+    });*/
 })
